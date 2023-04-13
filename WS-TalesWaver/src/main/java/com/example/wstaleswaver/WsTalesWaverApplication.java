3421c292ed6b9171b8c6419112c73cfef2556e43
giuseppekaligarealvarado <gkaligare@skgtecnologia.com>
2023-04-13 10:00:24 -0500

    [Inicializada de servicios]

---
+++ WS-TalesWaver/src/main/java/com/example/wstaleswaver/WsTalesWaverApplication.java
@@ -0,0 +1,13 @@
+package com.example.wstaleswaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsTalesWaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsTalesWaverApplication.class, args);
+    }
+
+}
---
+++ WS-TalesWaver/src/test/java/com/example/wstaleswaver/WsTalesWaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wstaleswaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsTalesWaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
