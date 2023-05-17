a83fff4d3152a4c5639b43747bf2d7beca277d6f
Code Noob <53632822+CodeNoo6@users.noreply.github.com>
2023-05-17 17:52:43 -0500

    Desarrollo de servicio Jugador
    CRUD sobre jugador en Spring Boot

---
+++ SRVSTalesWeaver/SRVSTalesWeaver/src/main/java/com/talesweaver/SRVSTalesWeaver/SrvsTalesWeaver.java
@@ -0,0 +1,13 @@
+package com.talesweaver.SRVSTalesWeaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvsTalesWeaver {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvsTalesWeaver.class, args);
+	}
+
+}
---
+++ SRVSTalesWeaver/SRVSTalesWeaver/src/main/java/com/talesweaver/SRVSTalesWeaver/model/Jugador.java
@@ -0,0 +1,137 @@
+package com.talesweaver.SRVSTalesWeaver.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "jugador")
+public class Jugador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "primer_nombre", nullable = false)
+    private String primerNombre;
+
+    @Column(name = "segundo_nombre")
+    private String segundoNombre;
+
+    @Column(name = "primer_apellido", nullable = false)
+    private String primerApellido;
+
+    @Column(name = "segundo_apellido")
+    private String segundoApellido;
+
+    @Column(name = "universidad", nullable = false)
+    private String universidad;
+
+    @Column(name = "carrera", nullable = false)
+    private String carrera;
+
+    @Column(name = "semestre")
+    private Integer semestre;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public Jugador() {
+    }
+
+    public Jugador(String primerNombre, String segundoNombre, String primerApellido, String segundoApellido,
+                   String universidad, String carrera, Integer semestre, String username, String password) {
+        this.primerNombre = primerNombre;
+        this.segundoNombre = segundoNombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.universidad = universidad;
+        this.carrera = carrera;
+        this.semestre = semestre;
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters y setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getUniversidad() {
+        return universidad;
+    }
+
+    public void setUniversidad(String universidad) {
+        this.universidad = universidad;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public Integer getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(Integer semestre) {
+        this.semestre = semestre;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
